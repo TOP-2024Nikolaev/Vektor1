40ec66e4bac61f30a6177cf430fe157d5d558320
TOP-2024Nikolaev <v.m.nikolaev142@gmail.com>
2024-10-14 18:31:33 +0700

    7.1.1.

--- 7.1.1.py
+++ 7.1.1.py
@@ -1,4 +1,8 @@
-string = input().split(" ")
+string = input()
 if (0 < int(string[0]) > 100 or  0 < int(string[1]) > 100):
 
     print("Введенные числа вне диапозона")
+else:
+    number = int(string[0])
+    if number % 3 == 0:
+        print("Fizz")
