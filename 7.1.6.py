246b048c0451e28c133ad454bffb781ead2630ab
TOP-2024Nikolaev <v.m.nikolaev142@gmail.com>
2024-10-14 20:14:29 +0700

    7.1.6

---
+++ 7.1.6.py
@@ -0,0 +1,14 @@
+
+
+
+
+status = 0
+if int((string[0]) > int(string[1]) > int(string[2]:)
+    or int(string[0]) > int(string[1]) > int(string[2])):
+status = 0
+if int((string[2]) > int(string[1]) > int(string[0]:)
+    or int(string[2]) > int(string[0]) > int(string[1])):
+status = 1
+if int((string[2]) > int(string[1]) > int(string[0]:)
+    or int(string[2]) > int(string[0]) > int(string[1])):
+status = 2
---
+++ 7.12.5.py
@@ -0,0 +1,21 @@
+string = input().split(" ")
+
+
+
+base = 200
+percent = 0
+premiya = 200
+zp = int(i)
+print(zp_all)
+
+for i in string:
+    zp = int(i)
+    if 0 < zp < 500:
+        percent = 0.03
+    elif 500 <= zp < 1000:
+        percent = 0.05
+    elif zp >= 1000:
+        percent = 0.08
+
+    if
+    print("Менеджер №1:" , base *(1 + percent) + premiya)
