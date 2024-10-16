ea7bcf8c14117cb147e307918266ccd3ff4e4dcc
TOP-2024Nikolaev <v.m.nikolaev142@gmail.com>
2024-10-16 19:59:34 +0700

    dz7

--- Test dz7.py
+++ Test dz7.py
@@ -2,8 +2,9 @@ string = input().split(" ")
 
 begin = int(string[0])
 end = int(string[1])
-
+result = ""
 for i in range(begin, end):
-    if i % 7 == 0:
+    if i % 7 == 0 and i != 0:
+        result += str(i) + ","
         print(i)
 
