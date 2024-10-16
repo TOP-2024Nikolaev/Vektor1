7edb21d8b1a315c3e1f37833941eb4081e5e1ca2
TOP-2024Nikolaev <v.m.nikolaev142@gmail.com>
2024-10-16 19:28:17 +0700

    16.2

--- 16.2.py
+++ 16.2.py
@@ -1,6 +1,11 @@
+k = 0
+count_max = 0
 for i in string:
     max_volume = max(volume_all)
     print(max_volume)
     if max_volume == int(i):
-        print("макс объем", i.index(i))
+        count_max +=1
+        print("Количество максимальныъ значаний:", count_max)
+        number[k] += premia / count_max
 
+k += 1
