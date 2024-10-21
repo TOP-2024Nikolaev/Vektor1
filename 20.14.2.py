3a25fbf6e82e3ba3be9203a32f34e9434d779cd1
TOP-2024Nikolaev <v.m.nikolaev142@gmail.com>
2024-10-21 19:46:59 +0700

    20.14.2

---
+++ 20.14.2.py
@@ -0,0 +1,12 @@
+a1 = 1
+a2 = 2
+a3 = 4
+a4 = 6
+a = ["sgfgs", "rgfgsf", "sgss", "srgs"]
+
+b = a[0] + a[1]
+print(b)
+for i in range(len(a)):
+    string = '1234'
+    a[i] = a[i] + string
+print(a)
--- 21.10.1.py
+++ 21.10.1.py
@@ -1,4 +1,4 @@
 string1 = "Первая строка, вторая строка!"
 print(len(string1) - 1)
 string2 = '+234'
-print(string1[::-1])
+print(string1,join(string2)
